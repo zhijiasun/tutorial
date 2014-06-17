abc5b60f6f83476299c14bc1145add44f6bf27be
zhijiasun <zhijiasun84@gmail.com>
2014-06-17 21:57:22 +0800

    modify cbv

--- cbv/urls.py
+++ cbv/urls.py
@@ -4,4 +4,5 @@ from views import *
 urlpatterns = patterns('',
         url(r'^testtemplateview/$',TestTemplateView.as_view()),
         url(r'^testredirectview/$',TestRedirectView.as_view()),
+        url(r'^testlistview/$',TestListView.as_view()),
     )
--- cbv/views.py
+++ cbv/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView,View,RedirectView
+from django.views.generic import DetailView,CreateView,UpdateView,DeleteView,ListView,FormView
 from cbv.forms import ContactForm
-from django.views.generic.edit import FormView
+from cbv.models import Article
 
 # Create your views here.
 
@@ -26,4 +27,12 @@ class ContactView(FormView):
     success_url = '/thanks/'
 
     def form_valid(self):
+        pass
+
+
+class TestListView(ListView):
+    model = Article
+    template_name = 'testlistview.html'
+
+    def get_context_data(self):
         pass
